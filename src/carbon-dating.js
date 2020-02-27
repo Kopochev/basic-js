4143efc882abdce0fa79f94d6f31c0d4689bb67b
Kopochev <copochev@yandex.ru>
2020-02-27 16:08:15 +0300

    feat: implement carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,34 @@
+/*Calculation formula 
+          result = ln(MODERN_ACTIVITY / sampleActivity) / k;
+          k = 0.693 / HALF_LIFE_PERIOD;
+*/
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample( sampleActivity ) {
+ const k = 0.693 / HALF_LIFE_PERIOD;
+ if(typeof(sampleActivity) !== 'string'){
+   return false;
+ }
+ //create dot for check amount '.'
+ let dot = 0;
+ 
+ let activityNumber = [];
+ for(let i = 0; i < sampleActivity.length; i++){
+   
+   if(sampleActivity[i] === '.'){
+     dot++;
+   }
+   if(dot > 1){
+    dot--;
+    continue;
+   }
+   activityNumber.push(sampleActivity[i]);
+ }
+ activityNumber = Number(activityNumber.join(''));
+ if(isNaN(activityNumber) || activityNumber <= 0 || activityNumber > 15){
+   return false;
+ }
+ let result = Math.ceil(Math.log(MODERN_ACTIVITY / activityNumber) / k);
+ return result
 };
