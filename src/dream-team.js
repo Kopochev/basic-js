85ff10fb370545ee2cb34505bdc674cbc3559147
Kopochev <copochev@yandex.ru>
2020-02-28 17:33:43 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,17 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(members == undefined || members == null || typeof(members) == 'number'){
+    return false;
+}
+  let nameTeam = [];
+  for(let i = 0; i < members.length; i++){
+    if(typeof(members[i]) != 'string' || typeof(members[i]) == 'object'){
+      continue;
+    }
+    
+    let trimMember = members[i].trim();//delete whitespaces
+    nameTeam.push(trimMember[0].toLowerCase());
+  } 
+
+  nameTeam = nameTeam.sort();
+  return nameTeam.join('').toUpperCase();
 };
