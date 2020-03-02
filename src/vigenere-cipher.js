00d24af732d16df3ebc217ff4a3ec51b503d5dcd
Kopochev <copochev@yandex.ru>
2020-03-02 20:30:55 +0300

    feat: implement vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,12 +1,92 @@
+const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function createFinalKey(message, key, finalVar) {
+    while (finalVar.length < message.length) {
+        for (let i = 0; i < key.length; i++) {
+            finalVar.push(key[i]);
+        }
+    }
+    finalVar.splice(message.length, finalVar.length - 1);
+    for (let j = 0; j < message.length; j++) {
+        if (message[j] === ' ') {
+            finalVar.splice(j, 0, ' ');
+        }
+    }
+    finalVar.splice(message.length, finalVar.length - 1);
+    return finalVar;
+}
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode) {
+        if (mode == false) {
+            this.mode = false;
+        } else {
+            this.mode = true;
+        }
+    }
+    encrypt(message, key) {
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let finalKey = [];
+        createFinalKey(message, key, finalKey);
+        finalKey = finalKey.join('');
+
+        let number = 0;
+        let result = '';
+
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.includes(message[i])) {
+                number = alphabet.indexOf(message[i]) + alphabet.indexOf(finalKey[i])
+                if (number < alphabet.length) {
+                    number -= alphabet.length;
+                    number += alphabet.length;
+                }
+                else if (number >= alphabet.length) {
+                    number -= alphabet.length;
+                }
+                result += alphabet[number];
+            } else {
+                result += message[i];
+
+            }
+        }
+        if (this.mode == false) {
+            result = result.split('').reverse().join('');
+            return result;
+        } else {
+            return result;
+        }
+
+
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    decrypt(message, key) {
+        message = message.toUpperCase().split('');
+        key = key.toUpperCase().split('');
+        let finalKey = [];
+        createFinalKey(message, key, finalKey);
+        finalKey = finalKey.join('');
+
+        let number = 0;
+        let result = '';
+
+        for (let i = 0; i < message.length; i++) {
+            if (alphabet.includes(message[i])) {
+                number = alphabet.indexOf(message[i]) - alphabet.indexOf(finalKey[i])
+                if (number < 0) {
+                    number += alphabet.length;
+                }
+                result += alphabet[number];
+            } else {
+                result += message[i];
+
+            }
+        }
+        if (this.mode == false) {
+            result = result.split('').reverse().join('');
+            return result;
+        } else {
+            return result;
+        }
     }
 }
 
