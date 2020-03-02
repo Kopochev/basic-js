8311afb0fae2632b3639e604ba035634747d167b
Kopochev <copochev@yandex.ru>
2020-03-02 11:06:50 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,33 @@
 const chainMaker = {
+
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let length = this.chain.length;
+    return length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-  },
+    let strValue = `( ${value} )`;
+    this.chain.push(strValue);
+    return this;
+    },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (typeof (position) !== 'number' || 
+      position > this.getLength() || position < 1) {
+      this.chain = [];
+      throw new Error();
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let finish = this.chain.slice();
+    this.chain = [];
+    return finish.join('~~');
   }
 };
 
