4e063545b229cdb5b5c2e35b4c2c7af57f5fb9fc
Kopochev <copochev@yandex.ru>
2020-03-02 15:56:33 +0300

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,46 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    str = String(str);
+    options.addition =  String(options.addition);
+    let defaultOptions  = {separator: '+', 
+    repeatTimes: 0,
+    addition: '',
+    additionRepeatTimes: 0,
+    additionSeparator:'|'};
+
+    if(!options.separator){
+        options.separator = defaultOptions.separator; 
+    }
+    if(!options.repeatTimes){
+        options.repeatTimes = defaultOptions.repeatTimes; 
+    }
+    if(!options.addition){
+        options.addition = defaultOptions.addition; 
+    }
+    if(!options.additionRepeatTimes){
+        options.additionRepeatTimes = defaultOptions.additionRepeatTimes; 
+    }
+    if(!options.additionSeparator){
+        options.additionSeparator = defaultOptions.additionSeparator; 
+    }
+    let resultString = '';
+    if(options.repeatTimes == 0 && options.additionRepeatTimes == 0){
+        resultString = str + options.addition;
+        return resultString;
+    }
+    for(let i = 0; i < options.repeatTimes; i++){
+        resultString += str;
+            for(let j = 0; j < options.additionRepeatTimes; j++){
+                resultString += options.addition;  
+                if (j < options.additionRepeatTimes - 1){
+                    resultString += options.additionSeparator;
+                }
+            }
+            
+        if( i < options.repeatTimes - 1){
+        resultString += options.separator; 
+        }
+
+    }
+    return resultString;
 };
   
