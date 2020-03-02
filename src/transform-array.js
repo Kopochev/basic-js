0ea9a76a5e3d5f61d8f80bd037b6a4bcbfaccea1
Kopochev <copochev@yandex.ru>
2020-03-02 09:39:09 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,36 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if(!Array.isArray(arr)){
+        throw new Error();
+    }
+
+    let resultArray = [];
+    for(let i = 0; i < arr.length; i++){
+        if (arr[i] == '--discard-next'){
+            if(i + 1 < arr.length){
+                i++;
+            }
+            continue;
+        }
+        
+        if(arr[i] == '--discard-prev'){
+            if(i  > 0){
+               resultArray.pop(); 
+            }
+            continue;
+        }
+        if(arr[i] == '--double-next'){
+            if(i + 1 < arr.length){
+                resultArray.push(arr[i+1]);  
+            }
+            continue;
+        }
+        if(arr[i] == '--double-prev'){
+            if(i - 1 > 0){
+                resultArray.push(arr[i-1])
+            }
+            continue;
+        }
+        resultArray.push(arr[i]);
+    }
+    return resultArray;
 };
